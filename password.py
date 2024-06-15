431d043b1b4d495371cf4da207421144d7d79039
jgt8168 <jgt8168@hotmail.com>
2024-06-15 14:12:50 +0800

    update password.py

--- password.py
+++ password.py
@@ -1,14 +1,15 @@
 password='a123456'
-count=1
+count=0
 while True:
-    if count<=3:
-        inpw=input("請輸入密碼:")
+    count+=1
+    inpw=input("請輸入密碼:")
+    if count<=3:        
         if inpw==password:
             print("登入成功")
             break
-        else :
-            count+=1
+        else :    
+            if count==3:
+                print("最多輸入3次密碼")
+                break        
             print("密碼錯誤!還有",3-count,"次機會")
-    else:
-        print("最多輸入3次密碼")
-        break
+   
